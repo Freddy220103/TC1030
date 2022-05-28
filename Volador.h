35d4042419c69bcd1b4b7582f7fb91a4ac913409
Fredy Gómez <44040347+Freddy220103@users.noreply.github.com>
2022-05-28 10:01:00 -0500

    Update Volador.h

--- Volador.h
+++ Volador.h
@@ -12,10 +12,9 @@ private:
 public:
     Volador(){};
     Volador(string nom, int vid, int fuerz, int rang):nombre(nom),vida(vid),fuerza(fuerz),rango(rang){};
-
-    void ataque(int *ptrvidadef);
-    void moverse();
+    
     void volar_y_huir();
     void curar(Monstruo *monstruo);
     void curarse();
 #endif //RAMPAGE_VOLADOR_H
+
