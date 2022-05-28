b9bf420d0a98ab96dd916e1bef6cfd8582257013
Fredy Gómez <44040347+Freddy220103@users.noreply.github.com>
2022-05-28 10:02:01 -0500

    Update Transporte.h

--- Transporte.h
+++ Transporte.h
@@ -8,12 +8,22 @@
 class Transporte:public Defensa{
 private:
     int rango_misil;
+    int cooldown=2;
     void activar_escudo();
     void moverse();
-    void lanzar_misil(Monstruo monstruo);
+    void disparar(int *ptrvidamons);
 public:
     Transporte():Defensa("Transporte de misiles", 90, 12, 12){};
     Transporte(string nombre, int vida, int fuerza, int rango, int misil)
             :Defensa(nombre,vida,fuerza,rango),rango_misil(misil){};
 };
+void Defensa::disparar(int *ptrvidamons){
+    if (cooldown%2=0){
+        *ptrvidamons-fuerza;
+    }
+    else{
+        cout<<"El transporte de misiles intentó atacar pero tiene que recargar"<<endl;
+    }
+};
 #endif
+
