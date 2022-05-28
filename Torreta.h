0c4e55af251b2487f701c2982a3d76ca7f74c16d
Fredy Gómez <44040347+Freddy220103@users.noreply.github.com>
2022-05-28 10:03:04 -0500

    Update Torreta.h

--- Torreta.h
+++ Torreta.h
@@ -1,18 +1,30 @@
 //
 // Creado por Alfredo Gómez Mendoza el 27/05/2022.
 //
-#ifndef RAMPAGE_TORRETA_H
-#define RAMPAGE_TORRETA_H
-#include "Defensa.h"
 
-class Torreta:public Defensa{
-private:
-    int turnos_congelamiento;
-    int retroceso;
+#ifndef DEFENSA_H
+#define DEFENSA_H
+#include "Monstruo.h"
+#include<iostream>
+using namespace std;
+
+class Defensa{
+protected:
+    string nombre;
+    int vida;
+    int fuerza;
+    int posicion[3];
+    int rango;
     void activar_escudo();
+    void disparar(int *ptrvidamons);
 public:
-    Torreta():Defensa("Torreta de hielo", 50, 2, 9){};
-    Torreta(string nombre, int vida, int fuerza, int rango, int turnos, int retro)
-    :Defensa(nombre,vida,fuerza,rango),turnos_congelamiento(turnos), retroceso(retro){};
+    Defensa(){};
+    Defensa(string nom, int vid, int fuerz, int rang):nombre(nom),vida(vid),fuerza(fuerz),rango(rang){};
+    int *ptrvidadef=&vida;
 };
+void Defensa::disparar(int *ptrvidamons){
+    *ptrvidamons-fuerza;
+};
+
+
 #endif
