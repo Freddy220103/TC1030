9662744b2a8c9f3fe57f35175eb608a71aaffc40
Fredy Gómez <44040347+Freddy220103@users.noreply.github.com>
2022-05-28 07:48:49 -0500

    Create Asesino.h

---
+++ Asesino.h
@@ -0,0 +1,18 @@
+//
+// Creado por Alfredo Gómez Mendoza el 27/05/2022.
+//
+
+#ifndef RAMPAGE_ASESINO_H
+#define RAMPAGE_ASESINO_H
+#include "Monstruo.h"
+class Asesino:public Monstruo{
+private:
+    int torres_destruidas;
+
+public:
+    Asesino(){};
+    Asesino(string nom, int vid, int fuerz, int rang):nombre(nom),vida(vid),fuerza(fuerz),rango(rang){};
+
+    void ataque(int *ptrvidadef);
+    void mareo(Defensa *defensa);
+#endif //RAMPAGE_ASESINO_H
