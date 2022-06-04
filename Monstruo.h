800454ef1f2ebf1aee7087d45ac28bf51db98911
Fredy Gómez <44040347+Freddy220103@users.noreply.github.com>
2022-06-03 20:26:10 -0500

    Update Monstruo.h

--- Monstruo.h
+++ Monstruo.h
@@ -1,12 +1,13 @@
 //
 // Creado por Alfredo Gómez Mendoza el 27/05/2022.
 //
+#ifndef MONSTRUO_H_
+#define MONSTRUO_H_
 
-#ifndef MONSTRUO_H
-#define MONSTRUO_H
 #include<iostream>
 #include "Ciudad.h"
-#include "Defensa.h";
+
+
 using namespace std;
 
 class Monstruo{
@@ -14,21 +15,103 @@ protected:
     string nombre;
     int vida;
     int fuerza;
-    int posicion[3];
     int rango;
 
 public:
     Monstruo(){};
     Monstruo(string nom, int vid, int fuerz, int rang):nombre(nom),vida(vid),fuerza(fuerz),rango(rang){};
-    int *ptrvidamons=&vida;
-    void ataque(int *ptrvidadef);
+    void ataque();
     void moverse();
     //void destruir(Ciudad *ciudad);
-    void estadísticas_personaje();
+    void estadisticas_monstruo();
+    void herirse();
+    string get_nombre();
+    void destruir_ciudad();
+    void ataque_especial();
 
 };
-
-void Monstruo::ataque(int *ptrvidadef){
-    *ptrvidadef-fuerza;
+string Monstruo::get_nombre() {
+    return nombre;
+}
+void Monstruo::ataque(){
+    cout<<"tu monstruo ha atacado"<<endl;
 };
+
+void Monstruo::destruir_ciudad() {
+    cout<<nombre<<" ha atacado la ciudad"<<endl;
+}
+void Monstruo::herirse(){
+    cout<<"Han herido tu monstruo"<<endl;
+
+    //vida-fuerza de la defensa
+};
+void Monstruo::estadisticas_monstruo(){
+    cout<<"Turno de monstruo:"<<nombre<<endl;
+    cout<<"     Vida: "<<vida<<endl;
+    cout<<"     Fuerza: "<<fuerza<<endl;
+    cout<<"     Rango: "<<rango<<endl;
+}
+
+void Monstruo::ataque_especial(){
+    cout<<"El ataque especial ha iniciado"<<endl;
+}
+class Asesino:public Monstruo {
+private:
+    int torres_destruidas;
+    int i;
+public:
+    Asesino(){};
+
+    Asesino(string nom, int vid, int fuerz, int rang) : Monstruo(nom, vid, fuerz, rang){
+        nombre=nom;
+        vida=vid;
+        fuerz=fuerza;
+        rango=rang;
+    };
+
+    void ataque();
+
+    void ataque_especial();
+};
+
+void Asesino::ataque(){
+    i=0;
+    int resultado;
+    while(torres_destruidas>=i)
+        //*ptrvidadef-fuerza;
+        resultado=fuerza+torres_destruidas;
+    cout<<"el asesino ha atacado"<<endl;
+    i++;
+};
+void Asesino::ataque_especial() {
+    cout<<get_nombre()<<" ha iniciado el ataque mareo"<<endl;
+}
+class Tanque:public Monstruo {
+private:
+    int rango_escudo;
+public:
+    Tanque(){};
+
+    Tanque(string nom, int vid, int fuerz, int rang) : Monstruo(nom, vid, fuerz, rang){
+        nombre=nom;
+        vida=vid;
+        fuerz=fuerza;
+        rango=rang;
+    };
+
+    void moverse();
+
+    void proteger();
+
+    void ataque_especial();
+};
+
+void Tanque::proteger(){
+    cout<<"El monstruo se ha protegido"<<endl;
+}
+
+void Tanque::ataque_especial() {
+    cout<<"El ataque destructor se ha iniciado"<<endl;
+}
 #endif
+
